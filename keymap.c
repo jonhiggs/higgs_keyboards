742b5e52ecdba42d04673911b4d319682ef862e7
Jon Higgs <jhiggs@eml.cc>
2019-04-25 14:15:17 +1000

    Make it compile

--- keymap.c
+++ keymap.c
@@ -25,15 +25,27 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH,       RSFT, UP,        \
       LCTL, LALT, LGUI, FN1,              SPC,              FN1,  RGUI,       LEFT, DOWN, RGHT \
     ),
-    KEYMAP(   // LAYER 1: Function1
+    /*KEYMAP(   // LAYER 1: Function1
       GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS,\
       TRNS, TRNS, TRNS, UP,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      \
       TRNS, TRNS, LEFT, DOWN, RGHT, TRNS, TRNS, INS,  HOME, PGUP, BSPC, TRNS,       TRNS,      \
       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, DEL,  END,  PGDN, TRNS,       TRNS, TRNS,      \
       TRNS, TRNS, TRNS, TRNS,             TRNS,             TRNS, TRNS,       TRNS, TRNS, TRNS \
     ),
+    KEYMAP(   // LAYER 2: Function2
+      GRV,  F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,   F10,  F11,  F12,  TRNS, TRNS,\
+      TRNS, TRNS, TRNS, UP,   TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,      \
+      TRNS, TRNS, LEFT, DOWN, RGHT, TRNS, TRNS, INS,  HOME, PGUP, BSPC, TRNS,       TRNS,      \
+      TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, DEL,  END,  PGDN, TRNS,       TRNS, TRNS,      \
+      TRNS, TRNS, TRNS, TRNS,             TRNS,             TRNS, TRNS,       TRNS, TRNS, TRNS \
+    ), */
 };
 
+/*
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_MOMENTARY(1),           // FN1 switch to layer 1
+    1 = ACTION_LAYER_MOMENTARY(1),           // FN1 switch to layer 1
+    2 = ACTION_LAYER_MOMENTARY(2),           // FN1 switch to layer 2
 };
+*/
+
+const uint16_t PROGMEM fn_actions[] = {};
